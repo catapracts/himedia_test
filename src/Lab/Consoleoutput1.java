833751afefbed576e435b204a32d1801ab33a125
catapracts <catapracts@gmail.com>
2023-11-21 15:57:06 +0900

    23.11.21.java연습파일

---
+++ src/Lab/Consoleoutput1.java
@@ -0,0 +1,20 @@
+package Lab;
+
+public class Consoleoutput1 
+{
+	public static void main(String[] args) 
+	{
+		String farthername ="abc";
+		String mothername ="def";
+		String myname ="ghi";
+		
+		int age = 30;
+		
+		double weight1 = 70.5;
+		double weight2 = 48.5;
+		
+		System.out.println(farthername + "의 몸무게는" + " " + weight1 + "입니다.");
+		System.out.println(mothername + "의 몸무게는" + " " + weight2 + "입니다.");
+		System.out.println(myname + "의 나이는" + " " + age + "입니다.");
+	}
+}
---
+++ src/ch01/ConsoleOutput1.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class ConsoleOutput1 
+{
+	public static void main(String[] args)
+	{
+		
+		
+	}
+
+}
