f4913cfef766d3d4bb6255d3b0314f675670f3af
catapracts <catapracts@gmail.com>
2023-11-21 18:19:35 +0900

    23.11.21.upload

---
+++ src/chap2/ex01/DataType.java
@@ -0,0 +1,28 @@
+package chap2.ex01;
+
+public class DataType {
+
+	public static void main(String[] args) 
+	{
+		int c=10, d=26;
+		System.out.println(c + " " + d);
+		
+		c=55; d=66;
+		System.out.println(c + " " + d);
+		
+		final int ABC = 1;
+		final double PI = 3.14;
+		System.out.println(ABC + " " + PI);
+		
+		boolean abc; abc = true;
+		long gg = -9_223_372_036_854_755_808L;
+		
+		char oo = 97;
+		char pp = '\u0061';
+		System.out.println(pp);
+		
+		
+		
+	}
+
+}
